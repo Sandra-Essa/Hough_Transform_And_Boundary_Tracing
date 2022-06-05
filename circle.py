4fc2f2d22c7a47f6620bf4df9db6719d323fbe9b
Sandra Essa <76664987+Sandra-Essa@users.noreply.github.com>
2022-06-06 00:09:44 +0200

    Delete circle.py

--- circle.py
+++
@@ -1,8 +0,0 @@
-
-def pic_circle():
-    # initialize our canvas as a 300x300 pixel image with 1 channel
-    canvas3 = np.zeros((300, 300, 1), dtype="uint8")
-    cv2.circle(canvas3, (30,50), 50, 1, -1)
-    plt.subplot(221), plt.imshow(canvas3, cmap='gray')
-    plt.title('Single Region'), plt.xticks([]), plt.yticks([])
-    return canvas3
